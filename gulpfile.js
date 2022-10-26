b270a828638724107755ab6e459b85a978d8d638
morozkostua <morozkostua@ya.ru>
2022-10-26 20:16:45 +0300

    add scrollSmooth js script

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,7 @@ var path = {
 			'node_modules/gsap/dist/gsap.js',
       'node_modules/swiper/swiper-bundle.min.js',
       'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js', // http://manos.malihu.gr/jquery-custom-content-scroller/
+			// 'src/assets/js/scrollSmooth.js',
       'src/assets/js/main.js',
     ],
     json: 'src/*.json',
